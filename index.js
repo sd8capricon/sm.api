efb5e43fdff42ebfdf4f4eb83837f35a76415ef1
Siddharth Dhaigude <sd8capricon@gmail.com>
2021-06-12 18:13:52 +0530

    cb to save

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const userRoutes = require('./routes/userRoutes');
 const app = express();
 PORT = process.env.PORT || 5000;
 
+app.use(express.urlencoded({ extended:true }));
 app.use(express.json);
 
 mongoose.connect('mongodb+srv://sd8capricon:WeAreNo.1@cluster0.ep8vh.mongodb.net/SecuredMessengerDB?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology:true, useCreateIndex:true});
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,12 +2,24 @@ const router = require('express').Router();
 const User = require('../models/userModel');
 
 router.post('/create', (req, res)=>{
+    console.log(req.body.username);
     const user = new User({
         username: req.body.username,
         password: req.body.pass,
         email: req.body.email
-    })
-    user.save();
+    });
+    user.save((user, err)=>{
+        if(err){
+            res.json({
+                err: err
+            })
+        }
+        else{
+            res.json({
+                user: user
+            })
+        }
+    });
 });
 
 module.exports = router;
