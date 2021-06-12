a66687cf84eef307a87bdfe5c5a2cfe822627c43
Siddharth Dhaigude <sd8capricon@gmail.com>
2021-06-12 17:19:05 +0530

    connected to MongoDB

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 PORT = process.env.PORT || 5000;
+
+mongoose.connect('mongodb+srv://sd8capricon:WeAreNo.1@cluster0.ep8vh.mongodb.net/SecuredMessengerDB?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology:true});
+
 
 app.listen(PORT, ()=>{
     console.log(`Server Listening on PORT ${PORT}`)
