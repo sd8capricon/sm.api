4242ed66eae85a2bfe8482e25413ee47fd8f68c1
Siddharth Dhaigude <sd8capricon@gmail.com>
2021-06-12 17:50:14 +0530

    added userRoutes

---
+++ routes/userRoutes.js
@@ -0,0 +1,13 @@
+const router = require('express').Router;
+const User = require('../models/userModel');
+
+router.post('/create', (req, res)=>{
+    const user = new User({
+        username: req.body.username,
+        password: req.body.pass,
+        email: req.body.email
+    })
+    user.save();
+});
+
+module.exports = router;
