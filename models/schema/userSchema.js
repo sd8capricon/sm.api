5cff061cf48efac93b9f919a8f4b052f1315f5ca
Siddharth Dhaigude <sd8capricon@gmail.com>
2021-06-12 17:39:40 +0530

    created userSchema

---
+++ models/schema/userSchema.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: [true, "This username is already taken"],
+        maxLength: [12, "maximum username length is 12"],
+        minLength: [3, "minimum username length is 3"]
+    },
+    pasword: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: [true, "This email is already in use"],
+        maxLength: [12, "maximum email length is 12"],
+        minLength: [3, "minimum email length is 3"]
+    },
+});
+
+module.exports = userSchema;
