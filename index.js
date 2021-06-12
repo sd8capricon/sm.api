0436ae094b55f89a18dc741e4abde4ba79ebe726
Siddharth Dhaigude <sd8capricon@gmail.com>
2021-06-12 17:59:16 +0530

    fixed errors in userRoutes

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
+//Routes 
+const userRoutes = require('./routes/userRoutes');
+
 const app = express();
 PORT = process.env.PORT || 5000;
 
 app.use(express.json);
 
-mongoose.connect('mongodb+srv://sd8capricon:WeAreNo.1@cluster0.ep8vh.mongodb.net/SecuredMessengerDB?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology:true});
+mongoose.connect('mongodb+srv://sd8capricon:WeAreNo.1@cluster0.ep8vh.mongodb.net/SecuredMessengerDB?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology:true, useCreateIndex:true});
 
+app.use('/user', userRoutes);
 
 app.listen(PORT, ()=>{
     console.log(`Server Listening on PORT ${PORT}`)
